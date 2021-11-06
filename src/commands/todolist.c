72f0a479d13eb6f661dd134dcc2316960be49f09
febryola <febryola0402@gmail.com>
2021-11-06 11:50:34 +0700

    update todolist

--- src/commands/todolist.c
+++ src/commands/todolist.c
@@ -1,14 +1,27 @@
 #include <stdio.h>
 #include "../modules/pesanan.h"
+#include "../modules/item.h"
 
 // TODO: periksa lagi format outputnya 
 
-void todolist(Pesanan L, Queue Q){ 
+void todolist(Pesanan *L, Queue *Q){ 
     /*Menampilkan daftar pesanan yang masuk ke to do list */
     printf("Pesanan pada TO Do List:\n");
     int i;
-    for (i = 0; i <= IDX_TAIL(Q); i++)
+    for (i = 0; i <= IDX_TAIL(*Q); i++)
     {
-        printf("%d. %c → %c (%c), time: %d\n", i,PICK_UP(L),DROP_OFF(L),ITEM(L),PTIME(L)); 
+        if (ITEM(*L) == "P"){
+            printf("%d. %c → %c (Perishable Item), sisa waktu: %d\n", i,PICK_UP(*L),DROP_OFF(*L),PTIME(*L)); 
+        }
+        else if (ITEM(*L) == "N"){
+            printf("%d. %c → %c (Normal Item)\n", i,PICK_UP(*L),DROP_OFF(*L));
+        }
+        else if (ITEM(*L) == "H"){
+            printf("%d. %c → %c (Heavy Item)\n", i,PICK_UP(*L),DROP_OFF(*L));
+        }
+        else{
+            printf("%d. %c → %c (VIP Item)\n", i,PICK_UP(*L),DROP_OFF(*L));
+        }
+        
     }
 }
