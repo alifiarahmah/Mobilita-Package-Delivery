8feeec22b3058960b9910d103bda5cffce3a7650
vito <vitosgh@gmail.com>
2021-11-09 01:02:29 +0700

    Add IN_PROGRESS command

--- src/commands/inprogress.c
+++ src/commands/inprogress.c
@@ -2,6 +2,21 @@
 
 #include "../modules/adt.h"
 
-void inProgress(Stack backpack) {
+void inProgress(Queue backpack) {
+    int idx;
+    int ctr;
 
+    printf("Pesanan yang sedang diantarkan: \n");
+    if (!isEmpty(backpack)) {
+        ctr = 1;
+        idx = IDX_HEAD(backpack);
+
+        while (idx != IDX_TAIL(backpack)) {
+            // TODO: Ubah tergantung quality item-nya
+            printf("%d. Normal Item (Tujuan: %c)", ctr, Name(DROP_OFF(backpack.buffer[idx])));
+            idx++;
+        }
+    } else {
+        printf("Pesanan kosong.");
+    }
 }
