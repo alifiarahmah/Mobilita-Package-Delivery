629392615211a692927ead3612b21161f2bc5814
Alifia Rahmah <alifiarahmah03@gmail.com>
2021-11-09 21:31:00 +0700

    add buy.c

--- src/commands/buy.c
+++ src/commands/buy.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
-#include "../modules/gadget.h"
-#include "../modules/listpos.h"
-// #include <gadgetlist.h> <- buat adt sendiri pakai list statis
+#include "../modules/listgadget.h"
 
 /*	BUY: Menampilkan gadget yang dapat dibeli, dan membelinya
 	Proses:
@@ -16,19 +14,6 @@
 	- Entah gimana di list gadget buat sudah terbeli atau tidak
 	- Confirm lagi, masih rada ngawang gadgetnya bebas dibeli berkali2 ato engga
 */
-void buy(int money){
-	// if(!isFull(Inventory)){
-		printf("Uang anda sekarang: %d Yen\n", money);
-		printf("Gadget yang tersedia:\n"); // iterasi pake list
-		printf("1. Kain Pembungkus Waktu (800 Yen)\n");
-		printf("2. Senter Pembesar (1200 Yen)\n");
-		printf("3. Pintu Kemana Saja (1500 Yen)\n");
-		printf("4. Mesin Waktu (3000 Yen)\n");
-		printf("Gadget mana yang ingin kau beli? (ketik 0 jika ingin kembali)\n");
-		printf("\nENTER COMMAND: ");
-		// mesin kata-in
-		
-	// } else {
-	// 	printf("Inventory anda penuh. Anda tidak bisa membeli gadget baru.\n");
-	// }
+void buy(int money, ListGadget inventory){
+	buyGadget(&inventory, money);
 }
