4b3fb40722e4fb3819591ae5d0baf5ac3da8876d
Alifia Rahmah <alifiarahmah03@gmail.com>
2021-11-10 10:18:20 +0700

    added comments to item.h as guides

--- src/modules/item.h
+++ src/modules/item.h
@@ -1,3 +1,10 @@
+/* Struktur data Item
+   Jenis tipe Item:
+   - Normal Item, type 'N', expired -1
+   - Heavy Item, type 'H', expired -1
+   - Perishable Item, tipe 'P', dengan expiry tertentu.
+*/
+
 #ifndef ITEM_H
 #define ITEM_H
 
