6aae9a291d03b46f598cd9d64773a9e23e5f6787
JevantJedidia <73844363+JevantJedidia@users.noreply.github.com>
2021-11-15 20:48:30 +0700

    read_file

--- src/read_file.c
+++ src/read_file.c
@@ -1,12 +1,6 @@
 #include <stdio.h>
 #include <math.h>
-#include "charmachine.h"
-#include "wordmachine.h"
-#include "matrix.h"
-#include "Map.h"
-#include "point.h"
-#include "pesanan.h"
-#include "item.h"
+#include "../modules/adt.h"
 
 boolean endWord;
 Word currentWord;
