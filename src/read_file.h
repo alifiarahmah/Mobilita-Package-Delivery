64109cca27b2455e8c849b1eae289455b0bff713
JevantJedidia <73844363+JevantJedidia@users.noreply.github.com>
2021-11-15 21:51:16 +0700

    read_file

--- src/read_file.h
+++ src/read_file.h
@@ -1,12 +1,6 @@
 #include <stdio.h>
 #include <math.h>
-#include "charmachine.h"
-#include "wordmachine.h"
-#include "matrix.h"
-#include "Map.h"
-#include "point.h"
-#include "pesanan.h"
-#include "item.h"
+#include "../modules/adt.h"
 
 /* membaca file input dan mengisi  matrix peta, adjacency, dan queue pesanan */
 void bacaFile (char namafile[], Matrix *peta, Matrix *adj, Queue *pesanan);
