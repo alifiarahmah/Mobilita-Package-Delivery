91fa73f5a8cd0b9c3e279441e39b6e8d64fd262e
alifiarahmah <alifiarahmah03@gmail.com>
2021-11-16 18:51:51 +0700

    bugfix buy

--- src/commands/buy.c
+++ src/commands/buy.c
@@ -2,6 +2,10 @@
 #include "../modules/adt.h"
 #include "commands.h"
 
+boolean isCommandValid(char command){
+    return (command == '0') || (command == '1') || (command == '2') || (command == '3') || (command == '4');
+}
+
 /* Proses: Menampilkan antarmuka pembelian gadget */
 /* I.S. Mobita berada di Headquarter, banyak gadget dalam inventory tidak melebihi capacity. */
 /*      Terdapat uang sebesar money yen, uang bisa kosong */
@@ -10,7 +14,7 @@
 void buy(ListGadget *l, int *money){
 	if(!isFullListGadget(*l)){ // masih bisa membeli gadget
         char command, gtype;
-        int cost;
+        int cost = 0;
 
 		printf("Uang anda sekarang: %d Yen\n", *money);
 		printf("Gadget yang tersedia:\n");
@@ -19,28 +23,37 @@ void buy(ListGadget *l, int *money){
 		printf("3. Pintu Kemana Saja (1500 Yen)\n");
 		printf("4. Mesin Waktu (3000 Yen)\n");
 		printf("Gadget mana yang ingin kau beli? (ketik 0 jika ingin kembali)\n");
+
 		printf("\nENTER COMMAND: ");
-        scanf("%c", &command);
-        if (command == '1'){
-            cost = 800; // Kain pembungkus waktu
-            gtype = 'A';
-        } else if (command == '2'){
-            cost = 1200; // Senter pembesar
-            gtype = 'B';
-        } else if (command == '3'){
-            cost = 1500; // Pintu kemana saja
-            gtype = 'C';
-        } else if (command == '4'){
-            cost = 3000; // Mesin waktu
-            gtype = 'D';
+        startWord();
+        while(!isCommandValid(currentWord.contents[0])){
+            printf("Command tidak valid!\n");
+            printf("Gadget mana yang ingin kau beli? (ketik 0 jika ingin kembali)\n");
+            printf("\nENTER COMMAND: ");
+            startWord();
         }
-        if (*money >= cost){
-            *money -= cost;
-            insertGadget(l, gtype);
-            printGadgetName(gtype);
-            printf(" berhasil dibeli.\n");
-        } else {
-            printf("Uang tidak cukup untuk membeli Gadget!\n");
+        if (currentWord.contents[0] != '0'){
+            if (currentWord.contents[0] == '1'){
+                cost = 800; // Kain pembungkus waktu
+                gtype = 'A';
+            } else if (currentWord.contents[0] == '2'){
+                cost = 1200; // Senter pembesar
+                gtype = 'B';
+            } else if (currentWord.contents[0] == '3'){
+                cost = 1500; // Pintu kemana saja
+                gtype = 'C';
+            } else if (currentWord.contents[0] == '4'){
+                cost = 3000; // Mesin waktu
+                gtype = 'D';
+            }
+            if (*money >= cost){
+                *money -= cost;
+                insertGadget(l, gtype);
+                printGadgetName(gtype);
+                printf(" berhasil dibeli.\n");
+            } else {
+                printf("Uang tidak cukup untuk membeli Gadget!\n");
+            }
         }
 	} else { // list gadget penuh
 		printf("Inventory anda penuh. Anda tidak bisa membeli gadget baru.\n");
