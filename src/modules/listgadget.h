6043fcfcb40da5ffc5df867680be340ccb6416d0
alifiarahmah <alifiarahmah03@gmail.com>
2021-11-16 23:52:36 +0700

    changed VAL_UNDEF to GADGET_UNDEF

--- src/modules/listgadget.h
+++ src/modules/listgadget.h
@@ -16,8 +16,7 @@
 	#define IDX_UNDEF -1
 #endif
 /* Indeks tak terdefinisi*/
-// FIXME: jadi VAL_UNDEF nya '0' atau -999?
-#define VAL_UNDEF '0'
+#define GADGET_UNDEF '0'
 /* Nilai elemen tak terdefinisi*/
 
 /* Definisi elemen dan koleksi objek */
