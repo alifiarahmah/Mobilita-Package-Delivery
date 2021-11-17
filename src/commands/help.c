203adaf0d452d70b9636b6320212c8e32dd347e9
Febryola Kurnia Putri <71137410+febryola@users.noreply.github.com>
2021-11-17 12:12:37 +0700

    update help.c

--- src/commands/help.c
+++ src/commands/help.c
@@ -4,16 +4,16 @@
 
 void help(boolean *canReturn) {
     printf("MENU HELP\n");
-    printf("1. MOVE -> Menampilkan pilihan lokasi serta pindah ke lokasi yang ada\n");
-    printf("2. PICK_UP -> Mengambil item jika ada pesanan yang harus diambil pada lokasi\n");
-    printf("3. DROP_OFF -> Mengantarkan item ke lokasi jika item di tumpukan teratas sesuai pesanan\n");
-    printf("4. MAP -> Menampilkan peta\n");
-    printf("5. TO_DO -> Menampilkan pesanan yang masuk To Do List\n");
+    printf("1. MOVE       -> Menampilkan pilihan lokasi serta pindah ke lokasi yang ada\n");
+    printf("2. PICK_UP    -> Mengambil item jika ada pesanan yang harus diambil pada lokasi\n");
+    printf("3. DROP_OFF   -> Mengantarkan item ke lokasi jika item di tumpukan teratas sesuai pesanan\n");
+    printf("4. MAP        -> Menampilkan peta\n");
+    printf("5. TO_DO      -> Menampilkan pesanan yang masuk To Do List\n");
     printf("6. IN_PROGESS -> Menampilkan pesanan yang sedang dikerjakan\n");
-    printf("7. BUY -> Menampilkan gadget yang dapat dibeli dan membelinya (Hanya dapat dipanggil di HQ)\n");
-    printf("8. INVENTORY -> Menampilkan list isi inventory\n");
-    printf("9. HELP -> Menampilkan menu ini\n");
+    printf("7. BUY        -> Menampilkan gadget yang dapat dibeli dan membelinya (Hanya dapat dipanggil di HQ)\n");
+    printf("8. INVENTORY  -> Menampilkan list isi inventory\n");
+    printf("9. HELP       -> Menampilkan menu ini\n");
     if(canReturn){
-        printf("10. RETURN -> Mengembalikan item di tumpukan teratas kembali ke posisi pick up");
+        printf("10. RETURN    -> Mengembalikan item di tumpukan teratas kembali ke posisi pick up");
     }
 }
