17f611f65fee7719bf0c2084e2311340574960b0
alifiarahmah <alifiarahmah03@gmail.com>
2021-11-17 16:17:43 +0700

    modified game complete mechanism

--- src/main.c
+++ src/main.c
@@ -68,83 +68,93 @@ int main(){
     Pesanan val;
 
     do{
-        printf("\n\n");
-        printf("Mobita berada di titik %c",MAT_ELMT(peta,Absis(posisi),Ordinat(posisi)));
-        TulisPOINT(posisi);
-        printf("\n");
-        printf("Waktu: %d\n", time);
-        printf("Uang yang dimiliki: %d Yen\n", money);
-        printf("ENTER COMMAND: ");
-        startWord();
-        for (int i = 0; i < currentWord.length; i ++) {
-            command[i] = currentWord.contents[i];
-        }
-        command[currentWord.length] = '\0';
-        clearscreen;
-
-        if (cekKataSama(command,"MOVE")){
-            move(adj,&posisi,&time,lBuilding,&incTime,saveTime,&timeSpeed);
-
-            // tiap pindah waktu, pindahin pesanan dari queue pesanan ke linkedlist todo
-            int i = 0;
-            while(TIME(HEAD(pesanan)) == time){
-                if (TYPE(ITEM(HEAD(pesanan))) == 'V') {
-                    VIP++;
-                }
-                dequeue(&pesanan, &val);
-                insertLastLL(&todo, val);
-                i++;
-            }
-
-            if (i != 0){
-                printf("%d pesanan baru masuk!",i);
-            }
-
-            // tiap pindah waktu, item yang lewat time limit perish
-            // perishExpiredItem(time, &inprogress, &tas);
-
-        }
-        else if (cekKataSama(command,"PICK_UP")){
-            pickUp(posisi, peta, &todo, &inprogress, &tas, &incTime, &timeSpeed, VIP);
-        }
-        else if (cekKataSama(command,"DROP_OFF")){
-            dropOff(&tas,&inprogress,posisi,&money,&output,&incTime,&timeSpeed,&canReturn,&VIP,&psnBerhasil);
-        }
-        else if (cekKataSama(command,"MAP")){
-            Map(adj,peta,posisi,todo,tas);
-        }
-        else if (cekKataSama(command,"TO_DO")){
-            todolist(todo);
-        }
-        else if (cekKataSama(command,"IN_PROGRESS")){
-            inProgress(inprogress);
-        }
-        else if (cekKataSama(command,"BUY")){
-            buy(posisi, peta, &gadgetInventory, &money);
-        }
-        else if (cekKataSama(command,"INVENTORY")){
-            inventory(&gadgetInventory,&time,&incTime,&saveTime,&tas);
-        }
-        else if (cekKataSama(command,"HELP")){
-            help(&canReturn);
-        }
-        else if (cekKataSama(command,"RETURN")){
-            returnToSender(&inprogress, &todo, &tas, &canReturn);
-        }
-        else if (cekKataSama(command,"EXIT")){
-            printf("TERIMA KASIH SUDAH BERMAIN\n");
-        }
-        else if ((isEmpty(pesanan)) && (isEmptyLL(todo)) && (isEmptyLL(inprogress)) && (isEmptyStack(tas)) && (Name(posisi) == '8')){
-            printf("TERIMA KASIH SUDAH BERMAIN\n");
-            printf("Semua pesanan berhasil dikirim.\n");
+        if ((isEmpty(pesanan)) && (isEmptyLL(todo)) && (isEmptyLL(inprogress)) && (isEmptyStack(tas)) && (Name(posisi) == '8')) {
+            printf("Game selesai!\n");
             break;
         }
-        else{
-            printf("Masukan command tidak valid, coba lagi!");
+        else {
+            printf("\n\n");
+            printf("Mobita berada di titik %c",MAT_ELMT(peta,Absis(posisi),Ordinat(posisi)));
+            TulisPOINT(posisi);
+            printf("\n");
+            printf("Waktu: %d\n", time);
+            printf("Uang yang dimiliki: %d Yen\n", money);
+            printf("ENTER COMMAND: ");
+            startWord();
+            for (int i = 0; i < currentWord.length; i ++) {
+                command[i] = currentWord.contents[i];
+            }
+            command[currentWord.length] = '\0';
+            clearscreen;
+
+            if (cekKataSama(command,"MOVE")){
+                move(adj,&posisi,&time,lBuilding,&incTime,saveTime,&timeSpeed);
+
+                // tiap pindah waktu, pindahin pesanan dari queue pesanan ke linkedlist todo
+                int i = 0;
+                while(TIME(HEAD(pesanan)) == time){
+                    if (TYPE(ITEM(HEAD(pesanan))) == 'V') {
+                        VIP++;
+                    }
+                    dequeue(&pesanan, &val);
+                    insertLastLL(&todo, val);
+                    i++;
+                }
+
+                if (i != 0){
+                    printf("%d pesanan baru masuk!",i);
+                }
+
+                // tiap pindah waktu, item yang lewat time limit perish
+                // perishExpiredItem(time, &inprogress, &tas);
+
+            }
+            else if (cekKataSama(command,"PICK_UP")){
+                pickUp(posisi, peta, &todo, &inprogress, &tas, &incTime, &timeSpeed, VIP);
+            }
+            else if (cekKataSama(command,"DROP_OFF")){
+                dropOff(&tas,&inprogress,posisi,&money,&output,&incTime,&timeSpeed,&canReturn,&VIP,&psnBerhasil);
+            
+                if ((isEmpty(pesanan)) && (isEmptyLL(todo)) && (isEmptyLL(inprogress)) && (isEmptyStack(tas)) && (Name(posisi) != '8')) {
+                    printf("Pesanan sudah habis. Silahkan kembali ke Headquarter.\n");
+                } 
+            
+            }
+            else if (cekKataSama(command,"MAP")){
+                Map(adj,peta,posisi,todo,tas);
+            }
+            else if (cekKataSama(command,"TO_DO")){
+                todolist(todo);
+            }
+            else if (cekKataSama(command,"IN_PROGRESS")){
+                inProgress(inprogress);
+            }
+            else if (cekKataSama(command,"BUY")){
+                buy(posisi, peta, &gadgetInventory, &money);
+            }
+            else if (cekKataSama(command,"INVENTORY")){
+                inventory(&gadgetInventory,&time,&incTime,&saveTime,&tas);
+            }
+            else if (cekKataSama(command,"HELP")){
+                help(&canReturn);
+            }
+            else if (cekKataSama(command,"RETURN")){
+                returnToSender(&inprogress, &todo, &tas, &canReturn);
+
+				if ((isEmpty(pesanan)) && (isEmptyLL(todo)) && (isEmptyLL(inprogress)) && (isEmptyStack(tas)) && (Name(posisi) != '8')) {
+                    printf("Pesanan sudah habis. Silahkan kembali ke Headquarter.\n");
+                } 
+            }
+            else if (cekKataSama(command,"EXIT")){
+                printf("TERIMA KASIH SUDAH BERMAIN\n");
+            }
+            else{
+                printf("Masukan command tidak valid, coba lagi!");
+            }
         }
     }while(!cekKataSama(command,"EXIT"));
     
-    printf("-------------Statistik Akhir-------------");
+    printf("\n-------------Statistik Akhir-------------\n");
     printf("Jumlah pesanan yang berhasil diantar: %d\n",psnBerhasil);
     printf("Waktu yang terlampaui: %d\n",time);
 
