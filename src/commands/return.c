cff5cb081b6d5237c322c8aa6a00d1717b25ba1b
JevantJedidia <jevantjedidia@gmail.com>
2021-11-17 23:02:15 +0700

    fix Return for Empty Stack

--- src/commands/return.c
+++ src/commands/return.c
@@ -7,19 +7,24 @@
 /*			Time limit perishable item akan di-reset */
 void returnToSender(LList *inprogress, LList *todo, Stack *tas, boolean *canReturn){
 	if(*canReturn){
+		if(!isEmptyStack(*tas)){
 		Pesanan val;
-		if ((TYPE(ITEM(INFO(FIRST(*inprogress)))) != 'V')){ // teratas bukan VIP Item
-			pop(tas, &val);
-			deleteFirst(inprogress, &val);
-			PTIME(val) = EXPIRED(ITEM(val)); // reset perish countdown time
-			insertLastLL(todo, val);
+			if ((TYPE(ITEM(INFO(FIRST(*inprogress)))) != 'V')){ // teratas bukan VIP Item
+				pop(tas, &val);
+				deleteFirst(inprogress, &val);
+				PTIME(val) = EXPIRED(ITEM(val)); // reset perish countdown time
+				insertLastLL(todo, val);
 
-			printItemType(ITEM(val));
-			printf(" berhasil dikembalikan ke Pick Up Point %c\n", Name(PICK_UP(val)));
+				printItemType(ITEM(val));
+				printf(" berhasil dikembalikan ke Pick Up Point %c\n", Name(PICK_UP(val)));
 
-			*canReturn = false;
-		} else { // teratas VIP Item
-			printf("VIP Item tidak bisa dikembalikan!\n");
+				*canReturn = false;
+			} else { // teratas VIP Item
+				printf("VIP Item tidak bisa dikembalikan!\n");
+			}
+		}
+		else{
+			printf("Tas kosong!\n");
 		}
 	} else { // canReturn = false
 		printf("Mobita tidak memliki Ability Return To Sender!\n");
